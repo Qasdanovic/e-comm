45a53e1e7609b16e7748b05d49914c7a97c4a5d2
Qasdanovic <oussqas2002@gmail.com>
2024-12-20 23:24:48 +0100

    add some refs to skeleton

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function Skeleton() {
   return (
     <div className="card" aria-hidden="true">
-        <img src="..." className="card-img-top" alt='...'/>
-        <div className="card-body">
-            <h5 className="card-title placeholder-glow">
-                <span className="placeholder col-6"></span>
-            </h5>
-            <p className="card-text placeholder-glow">
-                <span className="placeholder col-7"></span>
-                <span className="placeholder col-4"></span>
-                <span className="placeholder col-4"></span>
-                <span className="placeholder col-6"></span>
-                <span className="placeholder col-8"></span>
-            </p>
-            {/* <a href="#" className="btn btn-primary disabled placeholder col-6" tabIndex={-1}></a> */}
-        </div>
+      <div
+        className="card-img-top placeholder"
+        style={{ height: "200px", background: "#e9ecef" }}></div>
+      <div className="card-body">
+        <h5 className="card-title placeholder-glow">
+          <span className="placeholder col-6"></span>
+        </h5>
+        <p className="card-text placeholder-glow">
+          <span className="placeholder col-7"></span>
+          <span className="placeholder col-4"></span>
+          <span className="placeholder col-4"></span>
+          <span className="placeholder col-6"></span>
+          <span className="placeholder col-8"></span>
+        </p>
+        {/* <a href="#" className="btn btn-primary disabled placeholder col-6" tabIndex={-1}></a> */}
+      </div>
     </div>
   );
 }
